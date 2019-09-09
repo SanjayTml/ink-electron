dd714cc8f050cc3bc67eee2a6098ff994b8e2382
jk <1777980+falafeljan@users.noreply.github.com>
2019-09-09 13:48:48 +0200

    add directory selection dialog

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,46 +1,25 @@
-import electron from 'electron';
-import React, { useState, useEffect } from 'react';
+import { remote } from 'electron';
+import React, { useCallback } from 'react';
 import Head from 'next/head';
-import fetch from 'isomorphic-unfetch';
-import Person from '../components/Person';
-
-// prevent SSR webpacking
-const ipcRenderer = electron.ipcRenderer || false;
 
 const Home = () => {
-  const [people, setPeople] = useState([]);
-
-  useEffect(() => {
-    // componentDidMount()
-    if (ipcRenderer) {
-      const fn = async () => {
-        const response = await fetch(`${ipcRenderer.sendSync('get-base-url')}/api/people`);
-        if (response.status === 200) {
-          setPeople(await response.json());
-        }
-      };
-      fn();
-    }
-
-    return () => {
-      // componentWillUnmount()
-      if (ipcRenderer) {
-        // unregister it
-        ipcRenderer.removeAllListeners('ping-pong');
-      }
-    };
-  }, []);
+  const handleChooseRepository = useCallback(async () => {
+    console.log(
+      JSON.stringify(
+        await remote.dialog.showOpenDialog({
+          properties: ['openDirectory'],
+        })
+      )
+    );
+  });
 
   return (
     <React.Fragment>
       <Head>
         <title>Home - Nextron (api-routes)</title>
       </Head>
-      <ul>
-        {people.map((person, index) => (
-          <Person key={index} person={person} />
-        ))}
-      </ul>
+
+      <button onClick={handleChooseRepository}>Choose Repository</button>
     </React.Fragment>
   );
 };
