d244951fe4107a96e114bde3dfb4cc77164b8717
jk <1777980+falafeljan@users.noreply.github.com>
2019-09-11 14:00:46 +0200

    fix styled-components SSR issues w/ next

---
+++ renderer/.babelrc.js
@@ -0,0 +1,15 @@
+const dev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  presets: ['next/babel'],
+  plugins: [
+    [
+      'styled-components',
+      {
+        ssr: !dev,
+        displayName: dev,
+        preprocess: false,
+      },
+    ],
+  ],
+};
