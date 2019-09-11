dd9618d9329d47bc2495b7044c6561aad24dfcc3
jk <1777980+falafeljan@users.noreply.github.com>
2019-09-11 10:25:39 +0200

    advanced layout, added bootstrap

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -1,29 +1,22 @@
-module.exports = {
-  webpack: (config) => Object.assign(config, {
-    target: 'electron-renderer',
-  }),
-  async exportPathMap () {
-    // fetch our list of people, this allow us to dynamically generate the exported pages
-    const people = require('./data');
-    // or...
-    // const response = await fetch('https://your-external-domain.com/api/people');
-    // const people = await response.json();
+const withCSS = require('@zeit/next-css');
 
-    // tranform the list of people into a map of pages with the pathname `/people/:id`
-    const pages = people.reduce(
-      (pages, person) =>
-        Object.assign({}, pages, {
-          [`/people/${person.id}`]: {
-            page: '/person',
-            query: { id: person.id },
-          },
-        }),
-      {},
-    );
-
-    // combine the map of post pages with the home
-    return Object.assign({}, pages, {
-      '/home': { page: '/home' },
-    })
+module.exports = withCSS({
+  // for `@zeit/next-css` (https://github.com/zeit/next-plugins/issues/266#issuecomment-474721942)
+  cssLoaderOptions: {
+    url: false,
   },
-};
+
+  webpack: config =>
+    Object.assign(config, {
+      target: 'electron-renderer',
+    }),
+
+  async exportPathMap() {
+    return Object.assign(
+      {},
+      {
+        '/home': { page: '/home' },
+      }
+    );
+  },
+});
--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -1,6 +1,34 @@
 import { remote } from 'electron';
 import React, { useCallback } from 'react';
 import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
+import { Row, Col, H3, Button } from '@bootstrap-styled/v4';
+import BootstrapProvider from '@bootstrap-styled/provider/lib/BootstrapProvider';
+import { makeTheme } from 'bootstrap-styled/lib/theme';
+require('@ibm/plex/css/ibm-plex.css');
+
+const inkTheme = makeTheme({
+  '$font-family-sans-serif:':
+    "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif",
+  '$font-family-base': "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif",
+  '$text-color': '#fff',
+  '$headings-color': '#fff',
+  '$body-bg': '#000',
+  '$btn-primary-bg': '#446487',
+});
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    min-width: 100%;
+    min-height: 100%;
+    margin: 0;
+  }
+  
+  body {
+    background: #000;
+    color: #fff;
+  }
+`;
 
 const Home = () => {
   const handleChooseRepository = useCallback(async () => {
@@ -14,13 +42,26 @@ const Home = () => {
   });
 
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (api-routes)</title>
-      </Head>
+    <BootstrapProvider theme={inkTheme}>
+      <React.Fragment>
+        <GlobalStyle />
 
-      <button onClick={handleChooseRepository}>Choose Repository</button>
-    </React.Fragment>
+        <Head>
+          <title>ununu • Ink</title>
+        </Head>
+        <Row>
+          <Col md={12}>
+            <H3>Ink</H3>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={12}>
+            <Button onClick={handleChooseRepository}>Choose Repository</Button>
+          </Col>
+        </Row>
+      </React.Fragment>
+    </BootstrapProvider>
   );
 };
 
