dafaa031b96440865954a42b20d48c4153f7e359
jk <1777980+falafeljan@users.noreply.github.com>
2019-10-26 19:03:43 +0200

    fix lib code format

--- main/lib/store/project-store.js
+++ main/lib/store/project-store.js
@@ -1,43 +1,44 @@
-import { Store } from "./store";
+import { Store } from './store';
 
-const defaultDoc = "store";
-const objectName = "projects";
+const defaultDoc = 'store';
+const objectName = 'projects';
 const defaultValue = [];
 let projectStore;
 
 export function init() {
-	projectStore = new Store(defaultDoc, objectName, defaultValue);
-	return projectStore;
+  projectStore = new Store(defaultDoc, objectName, defaultValue);
+  return projectStore;
 }
 
 export function Project(id, name, path, type) {
-	this.id = id;
-	this.name = name;
-	this.path = path;
-	this.type = type;
+  this.id = id;
+  this.name = name;
+  this.path = path;
+  this.type = type;
 }
 
-export function append(newProject) {
-	var projects = projectStore.get();
-	if (newProject.id == null || newProject.name == null || newProject.path == null) {
-		throw new Error('invalid project id/name/path');
-		return projects;
-	}
-	projects.push(newProject);
-	projectStore.set(projects);
-	return projects;
+export function append(project) {
+  const projects = projectStore.get();
+  if (project.id == null || project.name == null || project.path == null) {
+    throw new Error('invalid project id/name/path');
+  }
+
+  projects.push(project);
+  projectStore.set(projects);
+
+  return projects;
 }
 
 export function list() {
-	return projectStore.get();
+  return projectStore.get();
 }
 
 export function reset() {
-	projectStore.set(defaultValue);
-	return list();
+  projectStore.set(defaultValue);
+  return list();
 }
 
 export function getByPath(path) {
-	var projects = projectStore.get();
-	return projects.find(project => project.path === path);
+  var projects = projectStore.get();
+  return projects.find(project => project.path === path);
 }
